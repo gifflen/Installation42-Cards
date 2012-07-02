e101096d786e02e1709e54d29a416388f5c2bf41
Bruce Stringer <Bruce.Stringer@sxc.com>
2012-07-02 12:02:31 -0400

    Added sqlserver settings

--- installation42/settings.py
+++ installation42/settings.py
@@ -12,9 +12,9 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'NAME': 'inst42',                      # Or path to database file if using sqlite3.
+        'USER': 'inst42',                      # Not used with sqlite3.
+        'PASSWORD': 'mmQjYRrnamXnWQNN',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
