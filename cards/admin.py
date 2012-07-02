b02b6a1843df70083bc97f37caad7d8aaa66a19c
Bruce Stringer <Bruce.Stringer@sxc.com>
2012-07-02 16:24:27 -0400

    Added Base admin site for models

---
+++ cards/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from cards.models import *
+
+admin.site.register(ActionType)
+admin.site.register(ActionTarget)
+admin.site.register(ActionType)
+admin.site.register(Deck)
+admin.site.register(Faction)
+admin.site.register(Ability)
+admin.site.register(Card)
+
