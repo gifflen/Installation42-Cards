9c62119ce74ecc9a9d20c032bcec2f00d044156b
root <root@web.teamweasel.com>
2012-07-02 22:40:10 +0400

    Added the use of wsgi Fixed static files

--- installation42/settings.py
+++ installation42/settings.py
@@ -4,6 +4,7 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
+	('Bruce Stringer','bruce@teamweasel.com'),
     # ('Your Name', 'your_email@example.com'),
 )
 
@@ -48,18 +49,18 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = '/production/installation42/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = 'media'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/production/installation42/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -117,7 +118,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+     'django.contrib.admindocs',
     'cards',
 )
 
