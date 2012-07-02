daa79a014c4a45b70eb1bdcba33af0fe08b28eea
Bruce Stringer <Bruce.Stringer@sxc.com>
2012-07-02 16:28:11 -0400

    Fixing issues with admin site

--- cards/admin.py
+++ cards/admin.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 from cards.models import *
 
 admin.site.register(ActionType)
-admin.site.register(ActionTarget)
-admin.site.register(Action)
-admin.site.register(Deck)
-admin.site.register(Faction)
-admin.site.register(Ability)
-admin.site.register(Card)
+#admin.site.register(ActionTarget)
+#admin.site.register(Action)
+#admin.site.register(Deck)
+#admin.site.register(Faction)
+#admin.site.register(Ability)
+#admin.site.register(Card)
 
